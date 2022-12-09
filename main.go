c165baebf2e9ada83bbd2aa65c7ec3660de9c3f7
Akash Kumar Saw <akashkumarsaw03@gmail.com>
2022-12-09 15:17:56 +0530

    Builded the Api

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	fullName string
+	userName string
+	email    string
+}
+
+type Post struct {
+	title  string
+	body   string
+	author User
+}
+
+var data []Post
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/post", getPost).Methods("GET")
+	router.HandleFunc("/post", addPost).Methods("POST")
+	router.HandleFunc("/post/{id}", updatePost).Methods("PUT")
+	router.HandleFunc("/post/{id}", deletePost).Methods("DELETE")
+	http.ListenAndServe(":8080", router)
+}
+
+func getPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "Application/json")
+	json.NewEncoder(w).Encode(data)
+}
+
+func addPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "Application/json")
+	var newPost Post
+	json.NewDecoder(r.Body).Decode(&newPost)
+	data = append(data, newPost)
+	json.NewEncoder(w).Encode(newPost)
+}
+
+func updatePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "Application/json")
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID failed to convert into Integer"))
+		return
+	}
+	if id >= len(data) {
+		w.WriteHeader(400)
+		w.Write([]byte("No such data found"))
+		return
+	}
+	var newPost Post
+	json.NewDecoder(r.Body).Decode(&newPost)
+	data[id] = newPost
+	json.NewEncoder(w).Encode(newPost)
+}
+
+func deletePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "Application/josn")
+	var idParam string = mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID failed to convert into Integer"))
+		return
+	}
+	if id >= len(data) {
+		w.WriteHeader(400)
+		w.Write([]byte("No such data found"))
+		return
+	}
+	data = append(data[:id], data[id+1:]...)
+	w.WriteHeader(200)
+}
